4e0e90bd5cb5915d65523a791eab48ee0a0221b5
Breno Rodrigues <brenocartoon@hotmail.com>
2021-09-01 22:35:59 -0300

    Add controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    LivrosController
+  ],
+  providers: [
+    AppService,
+    LivrosService
+  ],
 })
 export class AppModule {}
