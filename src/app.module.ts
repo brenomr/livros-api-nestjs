c26ddaee3f97b1d40122ae37259119cc33dfbff6
Breno Rodrigues <brenocartoon@hotmail.com>
2021-09-01 23:59:17 -0300

    Setting up database and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LivrosController } from './livros.controller';
+import { Livro } from './livros.model';
 import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'livros',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
   controllers: [
     AppController,
     LivrosController
